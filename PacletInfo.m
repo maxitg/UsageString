1bf6e9153a43b309f7736673563952820fd4e447
Maksim Piskunov <maxitg@icloud.com>
2019-02-04 18:48:50 -0600

    Create directory hierarchy and documentation.

---
+++ PacletInfo.m
@@ -0,0 +1,17 @@
+(* Paclet Info File *)
+
+(* created 2019/02/04*)
+
+Paclet[
+    Name -> "UsageString",
+    Version -> "1.0",
+    MathematicaVersion -> "11.3+",
+    Description -> "Small package implementing UsageString, which produces usage strings with nice italic font for arguments.",
+    Creator -> "Maksim Piskunov",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
