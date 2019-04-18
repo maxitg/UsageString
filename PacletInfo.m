0bcc3c11d2fcf1c688ed6e6611a63088e8b61dbb
Maksim Piskunov <maxitg@icloud.com>
2019-04-17 19:50:18 -0500

    Add build script to pack a paclet.

---
+++ PacletInfo.m
@@ -0,0 +1,8 @@
+Paclet[
+    Name -> "UsageString",
+    Version -> "1.0",
+    MathematicaVersion -> "12.0+",
+    Description -> "Small package implementing UsageString, which produces usage strings with nice italic font for arguments.",
+    Creator -> "Maksim Piskunov",
+    Extensions -> {{"Application", Context -> "UsageString`"}}
+]
